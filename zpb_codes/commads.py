f583630462c2ea17eae7dc0355e08f812f1019c9
TG-PushpaReju <97326699+TG-chembanreju@users.noreply.github.com>
2022-03-24 18:31:47 +0530

    Update commads.py

--- zpb_codes/commads.py
+++ zpb_codes/commads.py
@@ -1,6 +1,17 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.types import CallbackQuery
+import random
+
+
+
+ALL_PIC = [
+ "https://telegra.ph/file/c273ae2226885e757a886.jpg",
+ "https://telegra.ph/file/21eae28fe6ee6776ef0b9.jpg"
+
+
+
+
 
 START_MESSAGE= """ Hey {} This bot's work on progress⚙️
 """
@@ -9,7 +20,8 @@ START_MESSAGE= """ Hey {} This bot's work on progress⚙️
 
 @Client.on_message(filters.command("start"))
 async def start_msg(bot, msg):
-    await msg.reply_text(
+    await msg.reply_photo(
+        photo=random.choice(ALL_PIC),
         text=START_MESSAGE.format(message.from_user.mention),
         reply_markup=InlineKeyboardMarkup( [[
             InlineKeyboardButton("ᴜᴩᴅᴀᴛᴇᴢ ᴄʜᴀɴɴᴇʟ", url=https://t.me/ZPB_CODES)
