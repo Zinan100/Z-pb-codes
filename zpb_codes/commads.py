a1acd4b9602a0adc4009c9ae48f23e400c115710
TG-PushpaReju <97326699+TG-chembanreju@users.noreply.github.com>
2022-03-24 14:49:16 +0530

    Update commads.py

--- zpb_codes/commads.py
+++ zpb_codes/commads.py
@@ -9,7 +9,7 @@ from pyrogram.types import CallbackQuery
 @Client.on_message(filters.command("start"))
 async def start_msg(bot, msg):
     await msg.reply_text(
-        text=Start_Message.format(message.from_user.mention),
+        text=START_MESSAGE.format(message.from_user.mention),
         reply_markup=InlineKeyboardMarkup( [[
             InlineKeyboardButton("ᴜᴩᴅᴀᴛᴇᴢ ᴄʜᴀɴɴᴇʟ", url=https://t.me/ZPB_CODES)
            ]]
