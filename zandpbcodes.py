fc9b84e4be063b3695c8a72ae77885054da75685
moviespot <97530806+Zinan100@users.noreply.github.com>
2022-03-24 14:27:29 +0530

    Create zandpbcodes.py

---
+++ zandpbcodes.py
@@ -0,0 +1,12 @@
+from pyrogram import Client, filters
+
+Pbz=Client(
+    "Me and pushpa",
+    api_id="13160306",
+    api_hash="5023c40ea655bc2834e48888b17ccee8",
+    bot_token="5167503671:AAFEq9j3dzy_oRWZ3HLxlfT1sH-9dEIdhH8",
+    plugins=dict(root="zpb_codes")
+)
+
+
+Pbz.run()
